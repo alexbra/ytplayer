745a8b47c0afdc25663a02a33f89c19b287fe8c2
Alex Brazhenko <v-olbraz@microsoft.com>
2016-09-28 19:20:30 -0700

    update img

---
+++ app/data/mock-video.ts
@@ -0,0 +1,12 @@
+import { Video } from '../videos';
+
+export const VIDEOS: Video[] = [
+  { code: 1, name: 'Lamborghini Aventador / Megafactories (National Geographic)', url: '8TjScK-fMgc', top: true },
+  { code: 2, name: 'The Most Luxurious First Class Airlines', url: '5VqhIzigk1s', top: true },
+  { code: 3, name: 'Pelé: Birth of a Legend Official Trailer', url: 'XBrfxHOXsDE', top: true },
+  { code: 4, name: 'Oig Rig "The World’s Largest Oil Rig" – Big Bigger Biggest', url: '7w0xaHZGTT8', top: true },
+  { code: 5, name: 'Switchfoot - The Original', url: 'AiZiqKUp-_k', top: true },
+  { code: 6, name: 'The Trucks Discovery Science History Documentary HD', url: 'kUX0PHq1M7U', top: true },
+  { code: 7, name: 'MegaFactories Mack Truck National Geographic HD', url: 'F-vAQq_cK0I', top: false },
+  { code: 8, name: 'Angular 2 Tutorial and Sublime Text Tips -- LazyWeb #7', url: 'M6ZRJX-3Exw', top: false },
+];
